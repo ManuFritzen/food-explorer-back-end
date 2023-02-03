e39491f328def8e80075058e980ce428d3ec1f43
Emanuelle <manufritzenpro@gmail.com>
2023-02-03 20:57:59 -0300

    structuring project

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 
-const app = express();
+const routes = require("./routes");
 
-app.get("/", (request, response) => {
-    response.send("Hello, World!");
-})
+const app = express();
+app.use(express.json());
+
+app.use(routes);
+
+
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server in running on Port ${PORT}`));

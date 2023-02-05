bcd26a30f840d4cb2184f77344752c12886970c1
Emanuelle <manufritzenpro@gmail.com>
2023-02-05 13:57:41 -0300

    registering user

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,16 +1,22 @@
 const AppError = require("../utils/AppError");
 
+const sqliteConnection = require("../database/sqlite");
+
 class UsersController {
-    create(request, response){
+    async create(request, response){
         const { name, email, password } = request.body;
 
-        if(!name){
-            throw new AppError("Nome obrigatório");
+        const database = await sqliteConnection();
+        const checkUserExists = await database.get('SELECT * FROM users WHERE email = (?)', [email]);
+
+        if(checkUserExists) {
+            throw new AppError("Este email já está caastrado.");
         }
 
-        response.status(201).json({ name, email, password });
-    }
+        await database.run('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, password]);
 
+        return response.status(201).json();
+    }
 };
 
 module.exports = UsersController;
--- src/database/migrations/index.js
+++ src/database/migrations/index.js
@@ -1,4 +1,4 @@
-const sqliteConnection = require("../../sqlite");
+const sqliteConnection = require("../sqlite");
 
 const createUsers = require('./createUsers');
 

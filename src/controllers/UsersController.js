fb2bc623cf8fd4079e44af2d3daee52d5e28f40f
Emanuelle <manufritzenpro@gmail.com>
2023-02-06 23:20:03 -0300

    encrypted password

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,3 +1,4 @@
+const { hash } = require("bcryptjs");
 const AppError = require("../utils/AppError");
 
 const sqliteConnection = require("../database/sqlite");
@@ -13,7 +14,9 @@ class UsersController {
             throw new AppError("Este email já está caastrado.");
         }
 
-        await database.run('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, password]);
+        const hashedPassword = await hash(password, 8);
+
+        await database.run('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, hashedPassword]);
 
         return response.status(201).json();
     }

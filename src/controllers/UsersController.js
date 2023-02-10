2b66fcf951f1669b9cc65f0f4b3eca42760ef3a3
Emanuelle <manufritzenpro@gmail.com>
2023-02-09 22:28:51 -0300

    password update

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
 
 const sqliteConnection = require("../database/sqlite");
@@ -22,7 +22,7 @@ class UsersController {
     }
 
     async update(request, respoonse) {
-        const { name, email } = request.body;
+        const { name, email, password, old_password } = request.body;
         const { id } = request.params;
 
         const database = await sqliteConnection();
@@ -40,13 +40,28 @@ class UsersController {
         user.name = name;
         user.email = email;
 
+        if(password && !old_password){
+            throw new AppError("Informe a senha antiga para poder definir a senha nova.");
+        }
+
+        if(password && old_password){
+            const checkOldPassword = await compare(old_password, user.password);
+            
+            if(!checkOldPassword){
+                throw new AppError("A senha antiga não confere!");
+            }
+
+            user.password = await hash(password, 8);
+        }
+
         await database.run(`
             UPDATE users SET
             name = ?,
             email = ?,
+            passwors = ?,
             updated_at = ?
             WHERE id = ?`,
-            [user.name, user.email, new Date(), id]
+            [user.name, user.email, user.password, new Date(), id]
         );
 
         return response.status(200).json();
